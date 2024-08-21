5ad90918311570c374908e04ad917040694281cc
mounir achir <mistamounir02@gmail.com>
2024-08-22 00:01:53 +0200

    committing

--- constants/index.ts
+++ constants/index.ts
@@ -123,6 +123,30 @@ export const Frontend_skill: Skill[] = [
     height: 80,
   },
   {
+    skill_name: "Bootstrap",
+    Image: "/bootstrap.jpg",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express js",
+    Image: "/express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
     skill_name: "React",
     Image: "/react.png",
     width: 80,
@@ -146,40 +170,9 @@ export const Frontend_skill: Skill[] = [
     width: 80,
     height: 80,
   },
-  {
-    skill_name: "Bootstrap",
-    Image: "/bootstrap.jpg",
-    width: 80,
-    height: 80,
-  },
 ];
 
-export const Backend_skill: Skill[] = [
-  {
-    skill_name: "Node js",
-    Image: "/node-js.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Express js",
-    Image: "/express.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Mongo db",
-    Image: "/mongodb.png",
-    width: 40,
-    height: 40,
-  },
-  {
-    skill_name: "My SQL",
-    Image: "/mysql.png",
-    width: 70,
-    height: 70,
-  },
-];
+export const Backend_skill: Skill[] = [];
 
 export const Full_stack: Skill[] = []; // Define full stack skills here if needed
 
